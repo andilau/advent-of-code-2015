5ad9ce734a936f1e8ccbcfb8d9d65e03f59734f2
Andi Lau <andi@herrlau.de>
2021-12-08 20:43:43 +0100

    Add Day4 (The Ideal Stocking Stuffer)

---
+++ src/test/kotlin/days/Day4Test.kt
@@ -0,0 +1,22 @@
+package days
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("Day 4")
+class Day4Test {
+    @Nested
+    inner class Part1 {
+        @Test
+        fun `The MD5 hash of abcdef609043 starts with five zeroes`() {
+            assertThat(Day4("abcdef").partOne()).isEqualTo(609043)
+        }
+
+        @Test
+        fun `The MD5 hash of pqrstuv1048970 starts with five zeros`() {
+            assertThat(Day4("pqrstuv").partOne()).isEqualTo(1048970)
+        }
+    }
+}

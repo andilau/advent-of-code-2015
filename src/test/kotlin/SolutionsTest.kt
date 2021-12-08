0edf5a4d6fb0990ce45e585b178132f0448812fe
Andi Lau <andi@herrlau.de>
2021-12-08 18:15:53 +0100

    Add Day3 (Perfectly Spherical Houses in a Vacuum)

--- src/test/kotlin/SolutionsTest.kt
+++ src/test/kotlin/SolutionsTest.kt
@@ -1,5 +1,4 @@
-import days.Day1
-import days.Day2
+import days.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.DynamicTest
@@ -13,6 +12,7 @@ class SolutionsTest {
     fun testAdventOfCode() = listOf(
         Day1(InputReader.getInputAsString(1)) to Pair(138, 1771),
         Day2(InputReader.getInputAsList(2)) to Pair(1588178, 3783758),
+        Day3(InputReader.getInputAsString(3)) to Pair(2081, 2341),
     )
         .map { (day, answers) ->
             DynamicTest.dynamicTest("${day.javaClass.simpleName} -> ${answers.first} / ${answers.second}") {

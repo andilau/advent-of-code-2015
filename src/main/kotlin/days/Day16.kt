568ac43613e8ca3ad335c80983751f9b78e4f3b6
Andi Lau <andi@herrlau.de>
2022-01-24 13:41:34 +0100

    Add Day16 (Aunt Sue) cleanup

--- src/main/kotlin/days/Day16.kt
+++ src/main/kotlin/days/Day16.kt
@@ -29,7 +29,6 @@ class Day16(input: List<String>) : Puzzle {
                     it.properties.containsKey(key) &&
                             it.properties[key] != value
                 }
-                println("ants.size = ${this.size}")
             }
             this
         }.single().id
@@ -47,7 +46,6 @@ class Day16(input: List<String>) : Puzzle {
                                     it.properties[key] == value
                             }
                 }
-                println("ants.size = ${this.size}")
             }
             this
         }.single().id
@@ -56,17 +54,11 @@ class Day16(input: List<String>) : Puzzle {
     data class AntSue(val id: Int, val properties: Map<String, Int>) {
         companion object {
             fun from(line: String): AntSue {
-                /*Sue 1: cars: 9, akitas: 3, goldfish: 0
-                Sue 2: akitas: 9, children: 3, samoyeds: 9
-                Sue 3: trees: 6, cars: 6, children: 4
-                Sue 4: trees: 4, vizslas: 4, goldfish: 9
-                Sue 5: akitas: 9, vizslas: 7, cars: 5
-                */
-                val no = line.substringBefore(':').substringAfter("Sue ").toInt()
+                val id = line.substringBefore(':').substringAfter("Sue ").toInt()
                 val properties = line.substringAfter(": ")
-                    .split(", ").associate { it.split(": ", limit = 2).let { it.first() to it.last().toInt() } }
-
-                return AntSue(no, properties)
+                    .split(", ")
+                    .associate { it.split(": ", limit = 2).let { it.first() to it.last().toInt() } }
+                return AntSue(id, properties)
             }
         }
     }

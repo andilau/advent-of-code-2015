1d4d120fd1b3a7bbb2a657d6e278977c203e50df
Andi Lau <andi@herrlau.de>
2022-01-25 21:39:11 +0100

    Add Day17 (No Such Thing as Too Much) better

--- src/main/kotlin/days/Day17.kt
+++ src/main/kotlin/days/Day17.kt
@@ -8,17 +8,35 @@ package days
 class Day17(input: List<String>) : Puzzle {
     private val containers = input.map(String::toInt)
     internal var storage = 150
+    private val lists by lazy {
+        containers
+            .combinations()
+            .filter { it.sum() == storage }
+    }
 
-    override fun partOne() = containers
-        .combinations()
-        .filter { it.sum() == storage }
-        .onEach { println(it) }
+    private val fast by lazy { containers.combinationsSum(storage) }
+
+    override fun partOne() = fast
+        //.onEach { println(it) }
         .count()
 
-    override fun partTwo() = containers
-        .combinations()
-        .filter { it.sum() == storage }
+    override fun partTwo() = fast
         .groupingBy { it.size }.eachCount()
         .toSortedMap()
         .let { map -> map[map.firstKey()]!! }
+}
+
+ fun List<Int>.combinationsSum(target: Int): Sequence<List<Int>> = sequence {
+    if (target == 0)
+        yield(emptyList())
+    else {
+        val options = this@combinationsSum
+        options.withIndex().forEach { (index, container) ->
+            if (container <= target) {
+                subList(index + 1, options.size).combinationsSum(target - container).forEach {
+                    yield(listOf(container) + it)
+                }
+            }
+        }
+    }
 }
--- src/test/kotlin/days/Day17Test.kt
+++ src/test/kotlin/days/Day17Test.kt
@@ -14,13 +14,26 @@ class Day17Test {
     inner class Part1 {
         @Test
         fun test() {
-            assertThat(Day17(input).apply { storage=25 }.partOne()).isEqualTo(4)
+            assertThat(Day17(input).apply { storage = 25 }.partOne()).isEqualTo(4)
+        }
+
+        @Test
+        fun testFill() {
+            val toList = input.map(String::toInt).combinationsSum(25).toList()
+            toList.forEach { println(it) }
+            assertThat(toList)
+                .hasSize(4)
+                .containsExactlyInAnyOrder(listOf(20, 5), listOf(20, 5), listOf(15, 10), listOf(15, 5, 5))
         }
     }
 
     @Nested
     @DisplayName("Part 2")
     inner class Part2 {
+        @Test
+        fun `How many different ways to fill with  minimum number of containers`() {
+            assertThat(Day17(input).apply { storage = 25 }.partTwo()).isEqualTo(3)
+        }
 
     }
 }

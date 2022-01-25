bd721618717059cba6dd7cfe7bb41f8d0c50602a
Andi Lau <andi@herrlau.de>
2022-01-25 13:04:06 +0100

    Add Day16 (Aunt Sue) cleanup

--- src/main/kotlin/days/Day16.kt
+++ src/main/kotlin/days/Day16.kt
@@ -6,51 +6,46 @@ package days
     date = Date(day = 16, year = 2015)
 )
 class Day16(input: List<String>) : Puzzle {
-    private val aunts = input.map(AuntSue::from).toSet()
-    private val signature = measurements()
+    private val aunts = input.map(AuntSueDescription::from).toSet()
 
-    override fun partOne(): Int {
-        return with(aunts.toMutableSet()) {
-            signature.forEach { (key, value) ->
-                this.removeIf {
-                    it.properties.containsKey(key) &&
-                            it.properties[key] != value
-                }
+    override fun partOne(): Int = with(aunts.toMutableSet()) {
+        signature.forEach { (key, value) ->
+            this.removeIf {
+                it.properties.containsKey(key) &&
+                        it.properties[key] != value
             }
-            this
-        }.single().id
-    }
+        }
+        this
+    }.single().id
 
-    override fun partTwo(): Int {
-        return with(aunts.toMutableSet()) {
-            signature.forEach { (key, value) ->
-                this.removeIf {
-                    it.properties.containsKey(key) &&
-                            !when (key) {
-                                "cats", "trees" -> it.properties[key]!! > value
-                                "pomeranians", "goldfish" -> it.properties[key]!! < value
-                                else ->
-                                    it.properties[key] == value
-                            }
-                }
+    override fun partTwo(): Int = with(aunts.toMutableSet()) {
+        signature.forEach { (key, value) ->
+            this.removeIf {
+                it.properties.containsKey(key) &&
+                        !when (key) {
+                            "cats", "trees" -> it.properties[key]!! > value
+                            "pomeranians", "goldfish" -> it.properties[key]!! < value
+                            else ->
+                                it.properties[key] == value
+                        }
             }
-            this
-        }.single().id
-    }
+        }
+        this
+    }.single().id
 
-    data class AuntSue(val id: Int, val properties: Map<String, Int>) {
+    data class AuntSueDescription(val id: Int, val properties: Map<String, Int>) {
         companion object {
-            fun from(line: String): AuntSue {
+            fun from(line: String): AuntSueDescription {
                 val id = line.substringBefore(':').substringAfter("Sue ").toInt()
                 val properties = line.substringAfter(": ")
                     .split(", ")
                     .associate { it.split(": ", limit = 2).let { it.first() to it.last().toInt() } }
-                return AuntSue(id, properties)
+                return AuntSueDescription(id, properties)
             }
         }
     }
 
-    private fun measurements() = """
+    private val signature = """
              children: 3
              cats: 7
              samoyeds: 2
@@ -61,6 +56,7 @@ class Day16(input: List<String>) : Puzzle {
              trees: 3
              cars: 2
              perfumes: 1
-             """.trimIndent().lines()
+             """.trimIndent()
+        .lines()
         .associate { it.substringBefore(": ") to it.substringAfter(": ").toInt() }
 }

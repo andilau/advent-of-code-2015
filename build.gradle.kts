6f8724cc6fbd659454c27963c261554ae5a15449
Andi Lau <andi@herrlau.de>
2022-02-23 12:29:56 +0100

    Upgrade SLF4J-NOP binding to 1.7.36

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.reflections:reflections:0.10.2")
-    implementation("org.slf4j:slf4j-nop:1.7.35")
+    implementation("org.slf4j:slf4j-nop:1.7.36")
 
     testApi("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.assertj:assertj-core:3.22.0")

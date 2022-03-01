ab424cf211c9c206da2ec866984f90178293e3d2
Andi Lau <andi@herrlau.de>
2022-03-01 12:15:43 +0100

    Mod Day22 (Wizard Simulator 20XX) add tests

--- src/test/kotlin/days/Day22Test.kt
+++ src/test/kotlin/days/Day22Test.kt
@@ -1,32 +1,54 @@
 package days
 
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 @DisplayName("Day 22")
 class Day22Test {
-    val input = """
+
+    @Test
+    fun `What is the least amount of mana you can spend and still win the fight`() {
+        val input = """
         Hit Points: 51
         Damage: 9
         """.trimIndent().lines()
+        val day22 = Day22(input)
 
-    @Nested
-    @DisplayName("Part 1")
-    inner class Part1 {
-        @Test
-        fun `What is the least amount of mana you can spend and still win the fight`() {
-            assertThat(Day22(input).partOne()).isEqualTo(900)
+        with(SoftAssertions()) {
+            assertThat(day22.partOne()).isEqualTo(900)
+            assertThat(day22.partTwo()).isEqualTo(1216)
+            assertAll()
         }
     }
 
-    @Nested
-    @DisplayName("Part 2")
-    inner class Part2 {
-        @Test
-        fun `What is the least amount of mana you can spend and still win the fight in hard mode`() {
-            assertThat(Day22(input).partTwo()).isEqualTo(1216)
+    @Test
+    fun `Another Example 1`() {
+        val input = """
+        Hit Points: 58
+        Damage: 9
+        """.trimIndent().lines()
+        val day22 = Day22(input)
+
+        with(SoftAssertions()) {
+            assertThat(day22.partOne()).isEqualTo(1269)
+            assertThat(day22.partTwo()).isEqualTo(1309)
+            assertAll()
+        }
+    }
+
+    @Test
+    fun `Another Example 2`() {
+        val input = """
+            Hit Points: 71
+            Damage: 10            
+        """.trimIndent().lines()
+        val day22 = Day22(input)
+
+        with(SoftAssertions()) {
+            assertThat(day22.partOne()).isEqualTo(1824)
+            assertThat(day22.partTwo()).isEqualTo(1937)
+            assertAll()
         }
     }
 }

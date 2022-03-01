c50f84b290ad10d155336f028e3ce3f3a5a4137d
Andi Lau <andi@herrlau.de>
2022-03-01 11:04:00 +0100

    Mod Day25 (Let It Snow) improve tests, comments

--- src/main/kotlin/days/Day25.kt
+++ src/main/kotlin/days/Day25.kt
@@ -1,6 +1,5 @@
 package days
 
-
 @AdventOfCodePuzzle(
     name = "Let It Snow",
     url = "https://adventofcode.com/2015/day/25",
@@ -11,10 +10,12 @@ class Day25(prompt: String) : Puzzle {
     val col: Int
 
     init {
-        Regex("""\d+""").findAll(prompt).map(MatchResult::value).map(String::toInt).take(2).let {
-            row = it.first()
-            col = it.last()
-        }
+        Regex("""\d+""").findAll(prompt).map(MatchResult::value).map(String::toInt).toList().take(2)
+            .let { numbers ->
+                if (numbers.size < 2) error("Unable to extract row and column from prompt: $prompt")
+                row = numbers[0].also { number -> check(number > 0) }
+                col = numbers[1].also { number -> check(number > 0) }
+            }
     }
 
     override fun partOne() = codes().drop(index(row, col) - 1).first()
@@ -22,13 +23,28 @@ class Day25(prompt: String) : Puzzle {
     override fun partTwo() = Unit
 
     companion object {
-        // To continue, please consult the code grid in the manual.  Enter the code at row 2947, column 3029.
-        const val seed = 20151125L
+        private const val seed = 20151125L
 
-        internal fun index(row: Int, col: Int): Int = (row + col - 2) * (row + col - 1) / 2 + col
+        internal fun codes() = generateSequence(seed) { (it * 252533) % 33554393 }
 
-        internal fun codes() = generateSequence(seed) {
-            (it * 252533) % 33554393
-        }
+        /*
+        - side n = row + col - 2 (1-based)
+        - area t = n * (n + 1) / 2 (area of triangle)
+        - total index = t(#) + col(&)
+        -- n --
+        # # # #
+        # # #
+        # # &
+        # &
+        &
+        */
+
+        internal fun index(row: Int, col: Int): Int = triangular(row + col - 2) + col
+
+        /**
+         * https://en.wikipedia.org/wiki/Triangular_number
+         */
+
+        private fun triangular(n: Int) = n * (n + 1) / 2
     }
 }
